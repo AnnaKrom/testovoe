cb2ed9583e6f5b07dfb6c20b42eb49fd74f69be3
Anna Krom <anykrom-2012@yandex.ru>
2024-10-03 16:58:09 +0300

    add all files

---
+++ app_menu/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from app_menu.models import MenuItem
+
+
+def draw_menu(request, menu_name):
+    menu_items = MenuItem.objects.filter(name=menu_name).select_related('parent')
+    return render(request, 'menu.html', {'menu_items': menu_items})
